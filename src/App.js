ed841cee6430ed29a5c00dc339f867e1659c151f
colburncodes <colburnsanders@gmail.com>
2021-01-16 16:36:21 -0800

    functional  app

--- src/App.js
+++ src/App.js
@@ -1,47 +1,76 @@
 import React from "react";
+import "./index.css";
 
 export default class App extends React.Component {
-  // todos
   state = {
-    todos: [
-      {
-        userId: 1,
-        id: 1,
-        title: "delectus aut autem",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: "quis ut nam facilis et officia qui",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 3,
-        title: "fugiat veniam minus",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 4,
-        title: "et porro tempora",
-        completed: true,
-      },
-    ],
+    newTodo: "",
+    todos: [],
   };
+
+  toggleCompleted = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (id === todo.id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  createTodo = (e) => {
+    e.preventDefault();
+    if (this.state.newTodo !== "") {
+      const newTodo = {
+        userId: 1,
+        id: this.state.todos.length + 1,
+        title: this.state.newTodo,
+        completed: false,
+      };
+      this.setState({
+        todos: [...this.state.todos, newTodo],
+        newTodo: "",
+      });
+    }
+  };
+
+  componentDidMount() {
+    fetch(`https://jsonplaceholder.typicode.com/todos`)
+      .then((res) => res.json())
+      .then((data) => this.setState({ todos: data }));
+  }
+
   render() {
     return (
       <main>
         <div className="App">
           <h1>Colburn's Todo App</h1>
           <p># of Todos {this.state.todos.length}</p>
+          <form onSubmit={(e) => this.createTodo(e)}>
+            <input
+              type="text"
+              value={this.state.newTodo}
+              aria-label="New Todo"
+              placeholder="New Todo"
+              onChange={(e) => this.setState({ newTodo: e.target.value })}
+            />
+
+            <input type="submit" value="Add" aria-label="Add" />
+          </form>
+          <ul>
+            {this.state.todos
+              .sort((a, b) => b.id - a.id)
+              .map((todo, i) => (
+                <li
+                  key={i}
+                  className={`completed-${todo.completed}`}
+                  onClick={(e) => this.toggleCompleted(todo.id)}
+                >
+                  {todo.title}
+                </li>
+              ))}
+          </ul>
         </div>
-        <ul>
-          {this.state.todos.map((todo) => (
-            <li>{todo.title}</li>
-          ))}
-        </ul>
       </main>
     );
   }
